4b1ff61870d13364618b636cd9c793bf7ff6a7fd
Mizuki Morohoshi <8080suisho@gmail.com>
2020-12-10 15:38:39 +0900

    Initial Commit

---
+++ NewPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewPractice
+//
+//  Created by 諸星水晶 on 2020/12/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
